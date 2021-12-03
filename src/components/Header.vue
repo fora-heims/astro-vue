c69d89e999dbb66768ada9ea2f580d74ccd19caf
fora-heims <foraheims@gmail.com>
2021-12-03 11:45:26 -0800

    familiarized myself with the file structure and flow of a vue.js app. Added a header component to the page.

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">{{ name }}</div>
+</template>
+<script>
+export default {
+  name: "Header",
+  props: {
+    name: String,
+  },
+};
+</script>
+<style>
+.header {
+  background-color: #8b9eb7dd;
+  color: #91f5ad;
+  position: fixed;
+  top: 0%;
+  display: flex;
+  width: fill;
+  font-family: "Courier New", Courier, monospace;
+  font-weight: 700;
+  font-size: 24px;
+  padding: 5px;
+}
+</style>
